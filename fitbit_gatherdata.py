5c80464d0db1546586a369a98635afeb80beca5e
Eli <eli@elis-air.gateway.sonic.net>
2017-03-11 16:58:29 -0800

    Added weekly totals

--- fitbit_gatherdata.py
+++ fitbit_gatherdata.py
@@ -1,7 +1,36 @@
 import fitbit, json
-from datetime import date
 
+from datetime import date
 today = date.today()
+
+
+def calories_in():
+
+    print "CALORIES CONSUMED"
+
+    total_calories_in = 0
+
+    for calories_in_item in caloriesin['foods-log-caloriesIn']:
+        calories_in = calories_in_item['value']
+        print "%s - %s" % (calories_in_item['dateTime'],calories_in_item['value'])
+        total_calories_in += int(calories_in)
+
+    return total_calories_in
+
+
+def calories_out():
+
+    print "CALORIES BURNED"
+
+    total_calories_out = 0
+
+    for calories_out_item in caloriesout['activities-calories']:
+        calories_out = calories_out_item['value']
+        print "%s - %s" % (calories_out_item['dateTime'],calories_out_item['value'])
+        total_calories_out += int(calories_out)
+
+    return total_calories_out
+
 
 tokenfile = "user_settings.txt"
 
@@ -33,27 +62,20 @@ calories_out_today = caloriesout['activities-calories'][-1]['value']
 calories_in_today = caloriesin['foods-log-caloriesIn'][-1]['value']
 
 
+print '-----------------------'
+
 print "Welcome, %s!" % userprofile['user']['displayName']
 
 print "Today is: %s." % today
-
-print '-----------------------'
 
 print "Calories consumed so far today: %s" % calories_in_today
 print "Calories burned so far today: %s" % calories_out_today
 
 print '-----------------------'
 
-print "Calories consumed over the past week:"
-
-for calories_in_item in caloriesin['foods-log-caloriesIn']:
-    print calories_in_item['dateTime'], calories_in_item['value']
+print 'Calories consumed this week: %s' % calories_in()
 
 print '-----------------------'
 
-print "Calories burned over the past week:"
-
-for calories_out_item in caloriesout['activities-calories']:
-    print calories_out_item['dateTime'], calories_out_item['value']
-
+print 'Calories burned this week: %s' % calories_out()
 
